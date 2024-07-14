12bde19cf97f86e15e2cf896bced34d45caeb902
Zylo23 <pyyupsk@proton.me>
2024-07-14 15:52:19 +0700

    chore: update gitignore and Next.js configuration
    - Added git-status.log* to .gitignore
    - Enhanced next.config.js with basePath and reactStrictMode

--- next.config.js
+++ next.config.js
@@ -1,10 +1,15 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    reactStrictMode: true,
+    images: {
+        unoptimized: true,
+    },
+    basePath: '/docs',
+};
+
 const withNextra = require('nextra')({
     theme: 'nextra-theme-docs',
     themeConfig: './theme.config.tsx',
 });
 
-module.exports = withNextra({
-    images: {
-        unoptimized: true,
-    },
-});
+module.exports = withNextra(nextConfig);
